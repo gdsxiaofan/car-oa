6f805fbb3b49e6d4ded9cf90f38887b74c6ddcb9
gongdaoshun <597841337@qq.com>
2017-08-06 15:27:02 +0800

    修改启动端口号

--- oa-dao/src/main/java/com/xiaofan/car/AppStartConfiguration.java
+++ oa-dao/src/main/java/com/xiaofan/car/AppStartConfiguration.java
@@ -1,5 +1,6 @@
 package com.xiaofan.car;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by gongdaoshun on 2017/8/6.
  */
 @SpringBootApplication
+@MapperScan("com.xiaofan.car.dao.repository")
 public class AppStartConfiguration{
 
     public static  void main(String args[]){
--- oa-dao/src/test/java/com/xiaofan/car/dao/test/BaseDaoTest.java
+++ oa-dao/src/test/java/com/xiaofan/car/dao/test/BaseDaoTest.java
@@ -1,15 +1,19 @@
 package com.xiaofan.car.dao.test;
 
-import com.xiaofan.car.dao.configuration.MybatisConfiguration;
+import com.xiaofan.car.AppStartConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MybatisConfiguration.class)
+@SpringBootTest(classes = AppStartConfiguration.class)
 @Slf4j
+@Transactional
+@Rollback
 public class BaseDaoTest {
 
     @Test
