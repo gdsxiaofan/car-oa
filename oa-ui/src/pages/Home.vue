953ad3e72284c9fd31613bfc7ef996748b1469e3
duhongda <378935454@qq.com>
2017-08-26 12:01:51 +0800

    菜单支持1一级菜单

--- oa-ui/src/pages/Home.vue
+++ oa-ui/src/pages/Home.vue
@@ -1,109 +1,3 @@
-<style scoped>
-  .layout {
-    background: #f5f7f9;
-    position: relative;
-    overflow: hidden;
-    height: 100%;
-  }
-
-  .layout-breadcrumb {
-    padding: 10px 15px 0;
-  }
-
-  .layout-content {
-    min-height: 200px;
-    margin: 15px;
-    overflow: auto;
-    background: #fff;
-    border-radius: 4px;
-    height: 80%;
-  }
-
-  .layout-content-main {
-    padding: 10px;
-  }
-
-  .layout-copy {
-    text-align: center;
-    padding: 10px 0 20px;
-    color: #9ea7b4;
-  }
-
-  .layout-menu-left {
-    background: #464c5b;
-  }
-
-  .layout-header {
-    height: 60px;
-    background: #fff;
-
-  }
-
-  .layout-logo-left {
-    width: 90%;
-    height: 30px;
-    background: #5b6270;
-    border-radius: 3px;
-    margin: 15px auto;
-    line-height: 30px;
-    color: white;
-    text-align: center;
-    vertical-align: middle;
-    overflow: hidden;
-  }
-
-  .layout-ceiling-main a {
-    color: #9ba7b5;
-  }
-
-  .layout-hide-text .layout-text {
-    display: none;
-  }
-
-  .ivu-col {
-    transition: width .2s ease-in-out;
-  }
-
-  .ivu-row-flex {
-    height: 100%;
-  }
-
-  .userinfo {
-    display: inline-block;
-    float: right;
-  }
-
-  .userinfo .ivu-dropdown {
-    margin-top: 50px;
-  }
-
-  .ivu-dropdown {
-    margin-right: 25px;
-    margin-top: 22px;
-  }
-
-  .ivu-menu-submenu-title {
-    padding: 14px;
-  }
-
-  .head-img {
-    width: 100%;
-    height: 60px;
-    line-height: 60px;
-    float: right;
-    margin-top: -50px;
-  }
-
-  .head-img img {
-    border-radius: 20px;
-    margin: 10px 0px 10px 10px;
-    width: 40px;
-    height: 40px;
-    float: right;
-  }
-
-
-</style>
 <template>
   <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
     <Row type="flex">
@@ -114,7 +8,7 @@
             <!--<Icon type="paper-airplane" :size="logoSize" v-show="logoIsDisplay"></Icon>-->
             <h3>工单管理系统</h3>
           </div>
-          <template v-for="(item,index) in menu">
+          <template v-for="(item,index) in menuMain">
             <Submenu :name="item.id" v-if="item.order==2">
               <template slot="title">
                 <Icon :type="item.icon" :size="iconSize"></Icon>
@@ -127,12 +21,12 @@
                 </Menu-item>
               </template>
             </Submenu>
-            <!--<template v-if="item.leaf&&item.children.length>0">-->
-            <!--<Menu-item :name="item.children[0].path">-->
-            <!--<Icon :type="item.iconCls" :size="iconSize"></Icon>-->
-            <!--<span class="layout-text">{{item.children[0].name}}</span>-->
-            <!--</Menu-item>-->
-            <!--</template>-->
+            <template v-if="item.order!=2">
+            <Menu-item :name="item.id">
+            <Icon :type="item.icon" :size="iconSize"></Icon>
+            <span class="layout-text">{{item.name}}</span>
+            </Menu-item>
+            </template>
           </template>
         </Menu>
       </i-col>
@@ -273,6 +167,11 @@
           return 0;
         }
       },
+      menuMain(){
+        return this.menu.filter(item=>{
+          return item.parent_menu_id===0
+        })
+      },
       openNames() {
         return this.$route.path === '/' ? [] : this.menu.filter(item => {
           return item.href === this.$route.path
@@ -357,3 +256,109 @@
     }
   }
 </script>
+<style scoped>
+  .layout {
+    background: #f5f7f9;
+    position: relative;
+    overflow: hidden;
+    height: 100%;
+  }
+
+  .layout-breadcrumb {
+    padding: 10px 15px 0;
+  }
+
+  .layout-content {
+    min-height: 200px;
+    margin: 15px;
+    overflow: auto;
+    background: #fff;
+    border-radius: 4px;
+    height: 80%;
+  }
+
+  .layout-content-main {
+    padding: 10px;
+  }
+
+  .layout-copy {
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+  }
+
+  .layout-menu-left {
+    background: #464c5b;
+  }
+
+  .layout-header {
+    height: 60px;
+    background: #fff;
+
+  }
+
+  .layout-logo-left {
+    width: 90%;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    margin: 15px auto;
+    line-height: 30px;
+    color: white;
+    text-align: center;
+    vertical-align: middle;
+    overflow: hidden;
+  }
+
+  .layout-ceiling-main a {
+    color: #9ba7b5;
+  }
+
+  .layout-hide-text .layout-text {
+    display: none;
+  }
+
+  .ivu-col {
+    transition: width .2s ease-in-out;
+  }
+
+  .ivu-row-flex {
+    height: 100%;
+  }
+
+  .userinfo {
+    display: inline-block;
+    float: right;
+  }
+
+  .userinfo .ivu-dropdown {
+    margin-top: 50px;
+  }
+
+  .ivu-dropdown {
+    margin-right: 25px;
+    margin-top: 22px;
+  }
+
+  .ivu-menu-submenu-title {
+    padding: 14px;
+  }
+
+  .head-img {
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    float: right;
+    margin-top: -50px;
+  }
+
+  .head-img img {
+    border-radius: 20px;
+    margin: 10px 0px 10px 10px;
+    width: 40px;
+    height: 40px;
+    float: right;
+  }
+
+
+</style>
