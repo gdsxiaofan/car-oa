6bc9e2c0e59ac1fa9301dcbce2a55b7ef601cdfe
gongdaoshun <597841337@qq.com>
2017-12-07 21:16:40 +0800

    指定jdk1.8编译版本

--- oa-api/src/main/java/com/xiaofan/car/utilApi/FileController.java
+++ oa-api/src/main/java/com/xiaofan/car/utilApi/FileController.java
@@ -2,7 +2,6 @@ package com.xiaofan.car.utilApi;
 
 import com.sun.deploy.net.URLEncoder;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 
 /**
  * 提供模板下载的功能
@@ -27,10 +25,11 @@ public class FileController {
 
     /**
      * 下载工单设备上传信息模板
+     *
      * @param response
      */
-    @RequestMapping(value = "/download",method = RequestMethod.GET)
-    public void getTpmDeviceTemplateFile(HttpServletRequest request,HttpServletResponse response) {
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void getTpmDeviceTemplateFile(HttpServletRequest request, HttpServletResponse response) {
         try {
             String userAgent = request.getHeader("User-Agent");
 
@@ -44,7 +43,7 @@ public class FileController {
                 //非IE浏览器的处理：
                 filename = new String(filename.getBytes("UTF-8"), "ISO-8859-1");
             }
-                InputStream is = this.getClass().getResourceAsStream("/template/工单设备上传模板.xlsx");
+            InputStream is = this.getClass().getResourceAsStream("/template/工单设备上传模板.xlsx");
 
             // 设置response内容的类型
             response.setContentType("application/octet-stream;charset=utf-8");
@@ -53,17 +52,17 @@ public class FileController {
             /**
              * 将要下载的文件内容通过输出流写回到浏览器
              */
-                OutputStream os = response.getOutputStream();
-                int len = -1;
-                byte[] b = new byte[1024 * 100];
-                while ((len = is.read(b)) != -1) {
-                    os.write(b, 0, len);
-                    os.flush();
-                }
-                os.close();
-                is.close();
-            }catch(Exception e){
-                log.error("获取设备上传模板失败", e);
+            OutputStream os = response.getOutputStream();
+            int len = -1;
+            byte[] b = new byte[1024 * 100];
+            while ((len = is.read(b)) != -1) {
+                os.write(b, 0, len);
+                os.flush();
             }
+            os.close();
+            is.close();
+        } catch (Exception e) {
+            log.error("获取设备上传模板失败", e);
+        }
     }
 }
